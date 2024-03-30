11a3cbf0b2b79826846f5c15822bcd9d35b2dfb1
Mohammad Sahragard <mohammadsah4481@gmail.com>
2024-03-30 20:50:32 +0330

    Add dependencies (packages) and ready layout to start.

---
+++ app/providers.tsx
@@ -0,0 +1,23 @@
+// app/providers.tsx
+'use client';
+
+// next ui provider
+import { NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+// redux provider
+import { Provider } from 'react-redux';
+import store from '@/redux/app/store';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <NextUIProvider>
+            <NextThemesProvider
+                attribute='class'
+                defaultTheme='dark'
+            >
+                <Provider store={store}>{children}</Provider>
+            </NextThemesProvider>
+        </NextUIProvider>
+    );
+}
