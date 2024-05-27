4f44433f7d5ec714c97a551c3793364cd6bbc43c
Mohammad Sahragard <mohammadsah4481@gmail.com>
2024-05-27 21:09:22 +0330

    Fixed bugs. (the id of subtask items isn't unique)

--- models/task.ts
+++ models/task.ts
@@ -27,8 +27,8 @@ const taskSchema = new Schema({
     subtasks: [
         {
             _id: {
-                type: mongoose.Types.ObjectId,
-                default: new mongoose.mongo.ObjectId(),
+                type: Schema.Types.ObjectId,
+                default: () => new mongoose.Types.ObjectId(),
             },
             subtask_title: {
                 type: String,
